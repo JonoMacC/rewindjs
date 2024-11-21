56467202db19873cdc16d6e9ef0e714b2b4865ec
Jon MacCaull <jon@spiretrading.com>
2024-11-21 16:37:14 -0500

    refactor: decouple state management from core functionality.

---
+++ src/Rewind/StateManager.js
@@ -0,0 +1,51 @@
+export class StateManager {
+  #target;
+  #observe;
+  #accessor;
+
+  /**
+   * @typedef {Object} Accessor
+   * @property {Function} get - A function to get the state of the target.
+   * @property {Function} set - A function to set the state of the target.
+   */
+
+  /**
+   * A utility class for managing the state of a target object. Use options.observe to specify the properties
+   * to observe automatically on the target. Alternatively, provide custom getter and setter functions using
+   * options.accessor.
+   *
+   * @param {Object} target - The target object to manage.
+   * @param {Object} options - Options.
+   * @param {string[]} options.observe - Properties to observe on the target.
+   * @param {Accessor} options.accessor - An accessor that returns the state of the target.
+   */
+  constructor(target, options) {
+    this.#target = target;
+    this.#observe = new Set(options.observe);
+    this.#accessor = options.accessor;
+  }
+
+  get state() {
+    if (this.#observe.size === 0) {
+      return this.#accessor.get(this.#target);
+    }
+
+    return Array.from(this.#observe).reduce((state, prop) => {
+      state[prop] = this.#target[prop];
+      return state;
+    }, {});
+  }
+
+  set state(newState) {
+    if (this.#observe.size === 0) {
+      this.#accessor.set(this.#target, newState);
+      return;
+    }
+
+    Object.entries(newState).forEach(([key, value]) => {
+      if (this.#observe.has(key)) {
+        this.#target[key] = value;
+      }
+    });
+  }
+}
