6cfd4447dede20b69a0bc0c7ff90efb860469e47
Jon MacCaull <jon@spiretrading.com>
2024-12-10 15:34:21 -0500

    feat: remove custom state accessor option.

--- src/Rewind/StateManager.js
+++ src/Rewind/StateManager.js
@@ -1,44 +1,28 @@
-// Type definitions
-import './types.js';
-
 export class StateManager {
   #target;
   #observe;
-  #accessor;
 
   /**
-   * A utility class for managing the state of a target object. Use options.observe to specify the properties
-   * to observe automatically on the target. Alternatively, provide custom getter and setter functions using
-   * options.accessor.
+   * A utility class for managing the state of a target object. Use observe to specify the properties
+   * to observe automatically on the target.
    *
    * @param {Object} target - The target object to manage.
-   * @param {Object} options - Options.
-   * @param {string[]} options.observe - Properties to observe on the target.
-   * @param {Accessor} options.accessor - An accessor that returns the state of the target.
+   * @param {string[]} observe - Properties to observe on the target.
    */
-  constructor(target, options) {
+  constructor(target, observe) {
     this.#target = target;
-    this.#observe = new Set(options.observe);
-    this.#accessor = options.accessor;
+    this.#observe = new Set(observe);
   }
 
   get state() {
-    if (this.#observe.size === 0) {
-      return this.#accessor.get(this.#target);
+    const state = {};
+    for (const prop of this.#observe) {
+      state[prop] = Reflect.get(this.#target, prop);
     }
-
-    return Array.from(this.#observe).reduce((state, prop) => {
-      state[prop] = this.#target[prop];
-      return state;
-    }, {});
+    return state;
   }
 
   set state(newState) {
-    if (this.#observe.size === 0) {
-      this.#accessor.set(this.#target, newState);
-      return;
-    }
-
     Object.entries(newState).forEach(([key, value]) => {
       if (this.#observe.has(key)) {
         this.#target[key] = value;
