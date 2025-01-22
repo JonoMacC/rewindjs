7e0262cc5cad31bd0f2e835a1d94f935c0ff251f
Jon MacCaull <jon@spiretrading.com>
2025-01-22 14:15:33 -0500

    refactor: track change events with change handler method and `coalesce` option of rewind.

--- src/demos/composite_board/CompositeBoard/CompositeBoard.js
+++ src/demos/composite_board/CompositeBoard/CompositeBoard.js
@@ -4,6 +4,10 @@ import RewindTile from "../../tiles/RewindTile/RewindTile.js";
 // Utilities
 import cel from "../../../lib/celerity/cel.js";
 
+/**
+ * Class for a board that contains rewindable tiles. The board only concerns itself with the logic for adding and
+ * removing tiles. Because the tiles are rewindable, they handle their own undo/redo history internally.
+ */
 class BaseBoard extends HTMLElement {
   #keyMap = {
     insertKey: ["Shift+Enter"],
@@ -33,13 +37,13 @@ class BaseBoard extends HTMLElement {
   connectedCallback() {
     // Add event listeners
     this.addEventListener("keydown", this.#handleKeydown);
-    this.addEventListener("change", this.#handleChange);
+    this.addEventListener("change", this.onChange);
   }
 
   disconnectedCallback() {
     // Remove event listeners
     this.removeEventListener("keydown", this.#handleKeydown);
-    this.removeEventListener("change", this.#handleChange);
+    this.removeEventListener("change", this.onChange);
   }
 
   // Private methods
@@ -91,9 +95,25 @@ class BaseBoard extends HTMLElement {
     // Rewind state should handle recording automatically
     tile.remove();
   }
+
+  onChange() {
+    // Insert desired board logic here for a change event
+    // This setup is a little unrealistic as it is atypical to have an explicit user event trigger saving the undo state
+    // without a corresponding property change.
+  }
 }
 
+/**
+ * Class for a rewindable board with rewindable tiles. Because the board and tiles are rewindable, child management
+ * is handled by rewind. The `append` and `remove` methods called in BaseBoard are modified by rewind to record the
+ * state, enabling undo/redo without explicit tracking.
+ *
+ * The `onChange` method is explicitly tracked, as the board does not record a property but records on an event
+ * emitted by the tiles.
+ */
 /** @type {RewindableElementConstructor<BaseBoard>} */
-const CompositeBoard = rewind(BaseBoard);
+const CompositeBoard = rewind(BaseBoard, {
+  coalesce: ['onChange'],
+});
 
 customElements.define("gx-composite-board", CompositeBoard);
